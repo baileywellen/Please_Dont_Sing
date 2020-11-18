cc153a1c6dbca85fac3e8d5f56401b1929afd0fe
baileywellen <baileywellen@gmail.com>
2020-11-18 15:15:52 -0600

    optimized model
    used jupyter notebook to optimize model and began to transition the model to a .py script (implement_ML.py).

--- read_mp3.py
+++ read_mp3.py
@@ -7,11 +7,13 @@ Created on Mon Oct 26 09:05:36 2020
 
 from aubio import source, pitch
 import numpy as np
-import operator
+from scipy.stats import iqr
+
 
 
 def read_frequencies(filename):
     #we will use the original amount of samples in the file
+    #samplerate = 44100
     s = source(filename)    
     tolerance = 0.8
     
@@ -42,23 +44,73 @@ def read_frequencies(filename):
     s.close()
     
     return pitches, confidences
+ 
+#we will remove high outliers using the IQR * 1.5 rule for outliers.
+def remove_outliers(recording, confidences):
+    #cast to a numpy array
+    this_recording = np.array(recording)
     
-#here, we are trying to make them all the same length to be fed into the machine learning algorithm
-def make_arrays_same_length(pitches, confidences):
-    #f.write("RECORDING {} \n".format(recording_num))
-    #f.write(str(pitches))
-    #f.write("\n")
+    #find the IQR and the third quartile
+    my_iqr = iqr(this_recording)
+    q3 = np.quantile(this_recording, 0.75)
+
+    pitches_ret_val = []
+    confidences_ret_val  = []
     
-    # strip zeros from beginning and end of each list 
-    while (len(pitches) > 0) and (pitches[0] == 0.0) :
+    #remove any points that are above the third quartile by more than 1.5 * the IQR 
+    #we are only removing high outliers because those seem to be the result of aubio problems... we want to keep low outliers for now
+    for index in range(len(recording)):
+     #   print(recording)
+     if (recording[index] < (1.5 * my_iqr + q3)):
+         pitches_ret_val.append(recording[index])
+            
+         #keep the confidences in line with the pitches
+         confidences_ret_val.append(confidences[index])
+            
+            
+    return pitches_ret_val, confidences_ret_val
+  
+#remove unnecessary, inaccurate, or presumably unhelpful data
+def clean_arrays(pitches, confidences):
+    #first, if there are non-audible frequencies, we will remove those right away because we know they can't weigh into a person's perspective 
+    these_pitches = []
+    these_confidences = []
+    
+    #delete too high or too low notes
+    for index in range(len(pitches)):
+        #if the pitch is within a reasonable range (1567 is the highest pitch a human can sing, and 20 the lowest a human can hear), we will add it to a temporary list
+        if (pitches[index] > 20) and (pitches[index] < 1567):
+            these_pitches.append(pitches[index])
+            these_confidences.append(confidences[index])
+            index -= 1
+        
+    pitches = these_pitches
+    confidences = these_confidences
+    
+     #first, we will try to reduce some of the "noise" in the background of the recordings
+    these_pitches, these_confidences = remove_outliers(pitches, confidences)
+    
+    pitches = these_pitches
+    confidences = these_confidences
+    
+    
+    
+    # next, we will strip zeros from beginning and end of each list 
+    while (len(pitches) > 0) and (pitches[0] == 0) :
         pitches.pop(0)
         confidences.pop(0)
             
-    while (len(pitches) > 0) and  (pitches[-1] == 0.0) :
+    while (len(pitches) > 0) and  (pitches[-1] == 0) :
         pitches.pop(-1)
         confidences.pop(-1)
     
+    return pitches, confidences
     
+    
+
+#here, we are trying to make them all the same length to be fed into the machine learning algorithm
+def make_arrays_same_length(pitches, confidences):
+    pitches, confidences = clean_arrays(pitches, confidences)
     #if we need to delete any frequencies
     if len(pitches) > 1000:    
              
@@ -83,7 +135,7 @@ def make_arrays_same_length(pitches, confidences):
         total_added = 0 
         total_to_add = 1000 - len(pitches)
         last_confidence = 1
-        
+
         #we will add repeats of the frequencies with the highest confidence 
         while total_added < total_to_add:
             current_confidence = sum(confidences) / len(confidences)
@@ -111,64 +163,41 @@ def make_arrays_same_length(pitches, confidences):
             total_added += 1
             last_confidence = current_confidence
             
-    print('-------------------------------------------------------------------------------------------------------')
-            
-        #f.close()
     return pitches
   
-#loop through all of the files and read their frequency values
-all_pitches = []
-all_confidences = []
-
-#reading in all of the audio files and saving the frequencies in lists
-for num in range(0, 88):
-    filename = 'C://Users//baile//OneDrive//Desktop//Classes//Fall2020Classes//Thesis//Please_Dont_Sing//www//recording' + str(num) + '.mp3'
-    these_pitches, these_confidences = read_frequencies(filename)
-    all_pitches.append(these_pitches)
-    all_confidences.append(these_confidences)
-    
-
-f = open("C://Users//baile//OneDrive//Desktop//Classes//Fall2020Classes//Thesis//Please_Dont_Sing//complete_audio_freqs.csv", "w")
-
-csv_titles= []
-for i in range(1000):
-    csv_titles.append("frequency{}".format(i))
-
-f.write(','.join(csv_titles))
-f.write('\n')
-
-#making all of the arrays have the same length so they can be compared
-for recording_num in range(0,88):
-    pitches = all_pitches[recording_num]
-    confidences = all_confidences[recording_num]
+#call all of our other functions from here - takes the filename of an mp3 file, processes the audio, and returns a list of length 1000
+def get_freq_from_mp3(filename):
+    pitches, confidences = read_frequencies(filename)
     shortened_pitches = make_arrays_same_length(pitches, confidences)
-    shortened_pitches_strings = ["%.2f" % number for number in shortened_pitches]
-    print("Index {} has a length of {} ".format(recording_num, len(shortened_pitches_strings)))
-    f.write(','.join(shortened_pitches_strings))
+    return shortened_pitches
+    
+
+
+#read in the 88 training files and write them to a csv
+def read_training_data():
+    #write all of our training data to a csv file
+    f = open("C://Users//baile//OneDrive//Desktop//Classes//Fall2020Classes//Thesis//Please_Dont_Sing//complete_audio_freqs.csv", "w")
+    csv_titles= []
+    for i in range(1000):
+        csv_titles.append("frequency{}".format(i))
+    
+    f.write(','.join(csv_titles))
     f.write('\n')
-
-f.close()
     
-#The below section outputs the length, standard deviation, and average frequency of each recording
-#f = open("C://Users//baile//OneDrive//Desktop//Classes//Fall2020Classes//Thesis//Please_Dont_Sing//simplified_audio_freq.csv", "w")
-#f.write('id, average, std_dev, len')
-#for recording_num in range(0, 88):
-#    #find the average frequency of that recording
-#    average_freq = sum(all_pitches[recording_num]) / len(all_pitches[recording_num])
-#    #find the average frequency of that recording
-#    std_freq = np.std(np.array(all_pitches[recording_num]))
-#    
-#    #write those values to a file - recording id, average, std dev, and length
-#    f.write("{}, {}, {}, {} \n".format(recording_num, average_freq, std_freq, len(all_pitches[recording_num])))
-
+    for recording_num in range(0, 88):
+        filename = 'C://Users//baile//OneDrive//Desktop//Classes//Fall2020Classes//Thesis//Please_Dont_Sing//www//recording' + str(recording_num) + '.mp3'
+        cleaned_freqs = get_freq_from_mp3(filename)
+        cleaned_freqs_strings = ["%.2f" % number for number in cleaned_freqs]
+        f.write(','.join(cleaned_freqs_strings))
+        f.write('\n')
+        print("Wrote Index {} to file".format(recording_num))
+        
+    f.close()
     
-#f.close()
-            
 
-
-
-
-
+#we only want to run the last function when this file is run, NOT When it is imported
+if __name__ == "__main__":
+    read_training_data()
 
 
 
