ca0ee7b010e9c6d12efdf0c0cb1188f5fa39010e
baileywellen <baileywellen@gmail.com>
2020-12-14 16:40:07 -0600

    reverted old commit

--- pds_webapp/config.py
+++
@@ -1,8 +0,0 @@
-#Configuration File for the Flask WebApp
-import os 
-
-#create a class called Config
-class Config(object):
-    #confirm that nothing has been altered or hacked
-    SECRET_KY = os.environ.get('SECRET_KEY') or "secret_string"
-    
